c409977508074b4b11f127b9113cc1a139581140
Milkesa Tesema <milkesatesemawakwaya@gmail.com>
2026-03-06 22:29:06 +0300

    Add distinct split logic in 1791D - Distinct Split.py
    1791D - Distinct Split-Codeforce

---
+++ 1791D - Distinct Split.py
@@ -0,0 +1,18 @@
+from collections import Counter
+n = int(input())
+for _ in range(n):
+    length = int(input())
+    testcase = input()
+
+    left_set = set()
+    right_count =Counter(testcase)
+    total_sum = 0
+
+    for c in testcase:
+        left_set.add(c)
+        right_count[c] -= 1
+        if right_count[c] == 0:
+            del right_count[c]
+        total_sum = max(total_sum, len(left_set) + len(right_count))
+
+    print(total_sum)
