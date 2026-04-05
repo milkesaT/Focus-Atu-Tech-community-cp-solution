c9681f6c7166905a77c9a77888babaaaf74e1d29
Milkesa Tesema <milkesatesemawakwaya@gmail.com>
2026-04-05 22:47:45 +0300

    Update Contains duplicate.py

--- Sliding Window/Contains duplicate.py
+++ Sliding Window/Contains duplicate.py
@@ -1,1 +1,12 @@
 
+class Solution:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        i = 0
+        while i < len(nums):
+            j = i + 1  # start j from the next index
+            while j < len(nums):
+                if nums[i] == nums[j] and abs(i - j) <= k:
+                    return True
+                j += 1
+            i += 1
+        return False
